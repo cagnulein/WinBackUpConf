b3392498589c8c1595a33c63ae26336c045f22cb
Roberto Viola <roberto.viola@systemceramics.com>
2019-07-22 17:23:11 +0200

    save net drives

---
+++ WinBackUpConf/Configuration.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WinBackUpConf
+{
+    class Configuration
+    {
+        List<NetDrive> NetDrives = new List<NetDrive>();
+
+        public Configuration()
+        {
+            String[] netdrives = runCmd("net", "use");
+            foreach(String drive in netdrives)
+            {
+                NetDrive d = new NetDrive(drive);
+                if (d.IsValid())
+                    NetDrives.Add(d);
+            }
+        }
+
+        public Boolean Save(String filename)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+
+            XmlNode rootNode = xmlDoc.CreateElement("NetDrives");
+            xmlDoc.AppendChild(rootNode);
+            foreach (NetDrive d in NetDrives)
+                d.Save(xmlDoc, rootNode);
+            xmlDoc.Save(filename);
+
+            return true;
+        }
+
+        public String[] runCmd(String cmd, String arg)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = cmd;
+            startInfo.Arguments = arg;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            Process process = new Process();
+            process.StartInfo = startInfo;
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            string errors = process.StandardError.ReadToEnd();
+
+            char[] separator = { '\n' };
+            return output.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
---
+++ WinBackUpConf/NetDrive.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WinBackUpConf
+{
+    class NetDrive
+    {
+        private String path = "";
+        private String letter = "";
+
+        public String Path
+        {
+            get { return path; }
+            set { path = value; }
+        }
+
+        public String Letter
+        {
+            get { return letter; }
+            set { letter = value; }
+        }
+
+        public Boolean IsValid()
+        {
+            return path != "";
+        }
+
+        public void Parse(String line)
+        {
+            if (line.TrimStart().StartsWith("OK") == false) return;
+
+            char[] s = { ' ' };
+            String[] cols = line.Split(s, StringSplitOptions.RemoveEmptyEntries);
+            Letter = cols[1];
+            Path = cols[2];
+        }
+
+        public void Save(XmlDocument xmlDoc, XmlNode rootNode)
+        {
+            XmlNode userNode = xmlDoc.CreateElement("NetDrive");
+            XmlAttribute attribute = xmlDoc.CreateAttribute("path");
+            attribute.Value = Path;
+            userNode.Attributes.Append(attribute);
+            attribute = xmlDoc.CreateAttribute("letter");
+            attribute.Value = Letter;
+            userNode.Attributes.Append(attribute);
+            rootNode.AppendChild(userNode);
+        }
+
+        public NetDrive(String line)
+        {
+            Parse(line);
+        }
+    }
+}
